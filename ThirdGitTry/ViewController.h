33546668408adc48d7cbf0dfce9bfaea152442f7
CBA_Solution <cba_solutionMac2@CBA-Solutions-MacBook-Pro.local>
2015-08-10 13:13:49 -0700

    Initial Commit

---
+++ ThirdGitTry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirdGitTry
+//
+//  Created by CBA_Solution on 8/10/15.
+//  Copyright (c) 2015 CBA_Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
